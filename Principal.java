de1e5043955975336b5f656c0f44a45e4128b43e
jupilon <julianapilon2016@gmail.com>
2021-09-30 19:29:22 +0000

    heranca

--- Principal.java
+++ Principal.java
@@ -1,8 +1,16 @@
 public class Principal{
-    public static void main(String[] agrs) {
+    public static void main(String[] agr){
         
         Funcionario f=new Funcionario();
+
         f.setNome("Juliana");
+        f.setCPF("333.444.555-89");
+        f.setSalario(8900.00);
+        f.setDepartamento("Lideranca");
+
         System.out.println(f.getNome());
+        System.out.println(f.getCPF());
+        System.out.println(f.getSalario());
+        System.out.println(f.getDepartamento());
     }
 }
